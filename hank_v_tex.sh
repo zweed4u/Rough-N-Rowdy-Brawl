09f29929400481e28e2d8c3ebd9eebeb12f1ea80
Zachary Weeden <zackweeden@aol.com>
2017-12-15 23:48:59 -0500

    Earlier start

--- hank_v_tex.sh
+++ hank_v_tex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 stitch_string=''
-for i in `seq 1650 1720`;
+for i in `seq 1640 1720`;
 do
 	youtube-dl https://bcsecurelivehls-i.akamaihd.net/hls/live/567277/4289043293001/account=4289043293001/fd9b6917a8364cc2979d96d5b9b70228/4289043293001_8888_6b0_2500-0$i.ts
 	stitch_string+='4289043293001_8888_6b0_2500-0'$i'-4289043293001_8888_6b0_2500-0'$i'.ts '
