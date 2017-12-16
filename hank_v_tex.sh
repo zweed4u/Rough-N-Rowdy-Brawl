c42801297edbb3598071bd35a9d6c68142cd2923
Zachary Weeden <zackweeden@aol.com>
2017-12-15 23:47:48 -0500

    Stitching transport streams to one file

--- hank_v_tex.sh
+++ hank_v_tex.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+stitch_string=''
 for i in `seq 1650 1720`;
 do
-  youtube-dl https://bcsecurelivehls-i.akamaihd.net/hls/live/567277/4289043293001/account=4289043293001/fd9b6917a8364cc2979d96d5b9b70228/4289043293001_8888_6b0_2500-0$i.ts
+	youtube-dl https://bcsecurelivehls-i.akamaihd.net/hls/live/567277/4289043293001/account=4289043293001/fd9b6917a8364cc2979d96d5b9b70228/4289043293001_8888_6b0_2500-0$i.ts
+	stitch_string+='4289043293001_8888_6b0_2500-0'$i'-4289043293001_8888_6b0_2500-0'$i'.ts '
 done
+
+cat $stitch_string > HankVTex.ts
+ffmpeg -i HankVTex.ts -acodec copy -vcodec copy HankVTex.mp4
